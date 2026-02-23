2d9855f844caaad630af0f090026162c0a978ad0
Zaen1993 <nooralmostafa47@gmail.com>
2026-02-23 06:06:03 +0300

    Delete resource_analyzer.py

--- resource_analyzer.py
+++
@@ -1,5 +0,0 @@
-# resource_analyzer.py
-TASKS = {
-    'capture_log_state': {'description': 'Capture system log', 'category': 'system'},
-    'analyze_audio_feedback': {'description': 'Analyze microphone', 'category': 'system'},
-}
